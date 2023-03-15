ac541d3490e2ae0f3c5a4e39abdf4f69e58da16c
nypzlst <nyshtoprivet@gmail.com>
2023-03-15 15:12:13 +0200

    Work Portal
    ** Початкова версія порталу для переміщення на локацію
    ** Налаштування камери персонажа

---
+++ 2Dplatflormer/Assets/Scripts/Teleport.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Teleport : MonoBehaviour
+{
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Invoke("NextState", 5f);
+            // Добавить анимацию в хождение в портал чтобы скрыть косяки
+            Debug.Log("Code 1");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        CancelInvoke("NextState");
+        Debug.Log("Code 2");
+    }
+
+
+    void NextState()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
