a34b40cf73c5d5d6342de0abfc57ec231000e786
nypzlst <nyshtoprivet@gmail.com>
2023-05-18 14:48:40 +0300

    Update second level

--- 2Dplatflormer/Assets/Scripts/DestroyPlatform.cs
+++ 2Dplatflormer/Assets/Scripts/DestroyPlatform.cs
@@ -1,22 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
 
 public class DestroyPlatform : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public float timeToDestroy = 1f;
+    public float timeToDestroy ;
+    public float timeToRespawn = 3f;
+    public float timerStart = 3f;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+     void Start()
     {
-        if (collision.gameObject.CompareTag("Hero"))
+        timeToDestroy = timerStart;
+    }
+
+     void Update()
+    {
+        
+        if(timeToDestroy> 0)
         {
-            Invoke("OnDestroy", timeToDestroy);
+            timeToDestroy -= Time.deltaTime;
+        }
+        else if(timeToDestroy <= 0)
+        {
+            Debug.Log("time destroy");
+            gameObject.SetActive(false);
+            
+            Invoke("RespawnPlatform", timeToRespawn);
         }
     }
 
-    private void OnDestroy()
+    private void RespawnPlatform()
     {
-        Destroy(gameObject);
+        gameObject.SetActive(true);
+        timeToDestroy = timerStart;
     }
 }
